06499e7810f721e97fb788c3933fcb198a599d84
Marcel M. Cary <marcel@oreilly.com>
2012-05-01 16:40:19 -0700

    Pristine Rails 3.2.3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EnSsoExample::Application.config.secret_token = '6401b71a08bccfeffd855d2bc97e70d9039591da7707e964ecdee2de0e5cf928da37f588e8811e8cd4113f1b60d52f078e54426c73554993bb7f6887b8ab3574'
