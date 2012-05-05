562fb4973ca6d23c895895aab81628ec234340c7
Marcel M. Cary <marcel@oreilly.com>
2012-05-04 17:02:03 -0700

    Install omniauth-openid according to Devise wiki instructions

--- Gemfile
+++ Gemfile
@@ -25,6 +25,8 @@ gem 'jquery-rails'
 
 gem 'devise'
 
+gem 'omniauth-openid'
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,8 @@
+# When integrating with Devise, this happens in config/initializers/devise.rb
+#Rails.application.config.middleware.use OmniAuth::Builder do
+#  provider :open_id, :identifier => 'https://openid.orielly.com/'
+#end
+
+OmniAuth.config.logger = Rails.logger
+# We need to reach down to ruby-openid to configure a ca cert
+OpenID.fetcher.ca_file = Rails.root+"config/ca-certificates.crt"
