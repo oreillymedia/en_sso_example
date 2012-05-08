c04ff9837fc235a5a4b75bfa1c240ec48ce79b15
Marcel M. Cary <marcel@oreilly.com>
2012-05-08 10:46:36 -0700

    Skip :verify_authenticity_token filter on OpenID callback
    When a user returns from the OpenID server and hits the sso callback,
    the session is reset, and so the user_return_to URL is lost.  As a
    result, the callback redirects the user to the default location,
    root_url, instead of the original URL they tried to access when they
    began authentication.
    
    Skip the check of the authenticity_token: it's not useful in this case
    because the OpenID server won't be sending a token.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,4 +1,10 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  # Avoid resetting session during callback so that Devise can redirect back to
+  # the originally requested page.  This is really a GET request redirected
+  # from the OpenID endpoint anyway, so we're never going to be able to receive
+  # a token.
+  skip_before_filter :verify_authenticity_token, :only => [:sso]
+
   def sso
     @user = User.find_for_open_id(request.env["omniauth.auth"], current_user)
 
