9ca5617681370e96cca47d98cf0ee0b177334855
Marcel M. Cary <marcel@oreilly.com>
2013-02-11 15:44:29 -0800

    Generalize log messages in fetch_graph
    RdfApi#fetch_graph used to be just for Permissions, but now it's also
    for OPMI.  Its log messages mention permissions specifically.
    
    Remove the reference to permissions in favor of mentioning RDF graphs.
    (Also correct a capitalization error.)

--- app/models/rdf_api.rb
+++ app/models/rdf_api.rb
@@ -2,10 +2,10 @@ module RdfApi
   include HttpApi
 
   def fetch_graph(uri)
-    logger.debug "Fetching permissions from #{filter_uri_password(uri)}"
+    logger.debug "Fetching RDF from #{filter_uri_password(uri)}"
     rdf = fetch_body(uri)
 
-    benchmark "parse resulting graph" do
+    benchmark "Parse resulting graph" do
       # Since not loading from URL or file, we need to hint the serialization
       # format of the RDF.
       stmts = RDF::Reader.for(:rdfxml).new(rdf)
