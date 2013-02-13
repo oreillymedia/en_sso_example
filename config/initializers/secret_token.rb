bf753fb70c7858e35a777288d4c7f0d8aa65cde4
Marcel M. Cary <marcel@oreilly.com>
2013-02-13 15:37:52 -0800

    Extract the Rails secret_token to an environment variable
    This repository is open-source, so we must not store production
    secrets in the repository.
    
    Extract the secret_token used for signing sessions to an environment
    variable, and replace it with a newly generated token.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-EnSsoExample::Application.config.secret_token = '6401b71a08bccfeffd855d2bc97e70d9039591da7707e964ecdee2de0e5cf928da37f588e8811e8cd4113f1b60d52f078e54426c73554993bb7f6887b8ab3574'
+token = ENV['RAILS_SECRET_TOKEN']
+if token.blank?
+  raise "Please set RAILS_SECRET_TOKEN"
+end
+EnSsoExample::Application.config.secret_token = token
